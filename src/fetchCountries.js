1fdf82c015ce0b6fd49e29b7920f58e8bb630131
Mykhailo Volontyr <boommmeran@gmail.com>
2023-01-17 12:09:18 +0200

    add updates

---
+++ src/fetchCountries.js
@@ -0,0 +1,8 @@
+const BASE_URL = 'https://restcountries.com/v3.1/name/';
+
+function getCountryByName(name) {
+  return fetch(`${BASE_URL}${name}`).then(response => response.json(),
+  );
+};
+
+export default { getCountryByName };
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import './css/styles.css';
+import API from "./fetchCountries";
 
 const DEBOUNCE_DELAY = 300;
+
