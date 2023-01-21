2d3097984689482ec565a079261230d8c68dcdd2
Mykhailo Volontyr <boommmeran@gmail.com>
2023-01-21 18:38:30 +0200

    add some changes

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,14 +1,13 @@
-import Notiflix from 'notiflix'
-const API = 'https://restcountries.com/v3.1/name/'
-const filter = '?fields=name,capital,population,flags,languages'
+import Notiflix from 'notiflix';
+const API = 'https://restcountries.com/v3.1/name/';
+const filter = '?fields=name,capital,population,flags,languages';
 
 export function fetchCountries(name) {
-    return fetch(`${API}${name}${filter}`).then(response => {
-        if(!response.ok) {
-            Notiflix.Notify.failure('"Oops, there is no country with that name"')
-            throw new Error(response.status)
-      };
-      return response.json();
-    })
-    
+  return fetch(`${API}${name}${filter}`).then(response => {
+    if (!response.ok) {
+      Notiflix.Notify.failure('"Oops, there is no country with that name"');
+      throw new Error(response.status);
+    };
+    return response.json();
+  });
 };
